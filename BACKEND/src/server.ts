53c74916aac630e06c86e2b14c25fd033d4e105e
MohamedKhalifa7 <mohamedkhalifa4422@gmail.com>
2025-04-13 16:25:18 +0200

    change mongodb from local to atlas

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 // routers
 import authRouter from "./routes/authRouter";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use("/auth", authRouter);
 
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  console.error("MONGO_URI is not found");
+  process.exit(1);
+}
+
 mongoose
-  .connect("mongodb://localhost:27017/Smart-CV")
-  .then(() => console.log("mongo connected!"))
+  .connect(mongoUri)
+  .then(() => console.log("mongo Atlas connected!"))
   .catch(() => console.log("failed to connect!"));
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
