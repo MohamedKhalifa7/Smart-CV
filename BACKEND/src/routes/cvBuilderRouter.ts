1badbfc9649d483739f93c01fe67d475947a711d
MohamedKhalifa7 <mohamedkhalifa4422@gmail.com>
2025-04-20 15:09:34 +0200

    Add cv management

--- BACKEND/src/routes/cvBuilderRouter.ts
+++ BACKEND/src/routes/cvBuilderRouter.ts
@@ -9,19 +9,14 @@ import {
 
 const router = Router();
 
-// Create CV
 router.post("/save", saveCV);
 
-// Get all CVs for a user
 router.get("/user/:userId", getUserCVs);
 
-// Get a single CV by ID
 router.get("/:cvId", getCV);
 
-// Update a CV by ID
 router.put("/:cvId", editCV);
 
-// Delete a CV by ID
 router.delete("/:cvId", removeCV);
 
 export default router;
