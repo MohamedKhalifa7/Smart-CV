eca219271d3e2574a068762ab7e9485eff606fa8
Arwa Ayman <aymanarwa05@gmail.com>
2025-04-22 20:49:40 +0200

    Make Grammar Checker responsive

--- FRONTEND/src/componants/GrammarCheck/grammarCheck.jsx
+++ FRONTEND/src/componants/GrammarCheck/grammarCheck.jsx
@@ -1,11 +1,12 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 const GrammarCheck = () => {
     const [content, setContent] = useState("")
     const navigat = useNavigate();
-
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'))
     const handleContentChange = (event) => {
         setContent(event.target.value);
     }
@@ -16,7 +17,7 @@ const GrammarCheck = () => {
         <>
             {/* Header Section */}
             <Box sx={{ display: "flex", height: "20vh", width: "70vw", alignSelf: "center", justifyContent: "center", flexDirection: "column", m: "auto", mt: 3, }}>
-                <Button variant="text" sx={{ display: "flex", alignSelf: "start", margin: "10px", color: "text.secondary" }} onClick={() => navigat("/getStart")}
+                <Button variant="text" sx={{ display: "flex", alignSelf: "start", mt: "12px", color: "gray" }} onClick={() => navigat("/getStart")}
                     startIcon={<ArrowBackIcon></ArrowBackIcon>}>
                     Back to Get Started
                 </Button>
@@ -28,15 +29,15 @@ const GrammarCheck = () => {
                 }}>
                     Grammar & Style Checker
                 </Typography>
-                <Typography sx={{ margin: "auto", fontSize: "17px", color: "text.secondary" }}>
-                    Perfect your CV content with our advanced grammar and style checker</Typography>
+                {!isMobile && (<Typography sx={{ margin: "auto", fontSize: "16px", color: "gray" }}>
+                    Perfect your CV content with our advanced grammar and style checker</Typography>)}
             </Box>
             {/* Main Content Section */}
             <Box sx={{
-                display: "flex", height: "60vh", width: "70vw", alignSelf: "center",
-                flexDirection: "row", m: "auto", mt: 5, gap: 2, justifyContent: "space-between",
+                display: "flex", minHeight: "60vh", width: "70vw", alignSelf: "center",
+                flexDirection: isMobile ? "column" : "row", m: "auto", mt: 5, gap: 2, justifyContent: "space-between",
             }}>
-                <Box sx={{ width: "60%" }}>
+                <Box sx={{ width: isMobile ? "90%" : "60%" }}>
                     <TextField
                         multiline
                         rows={12}
@@ -54,12 +55,12 @@ const GrammarCheck = () => {
                         </Button>
                     </Box>
                 </Box>
-                <Box sx={{ width: "35%", alignSelf: "flex-start", minHeight: "150px", bgcolor: "background.gray", borderRadius: "10px" }}>
-                    <Typography sx={{ fontSize: "16px", fontWeight: "bold",p: 2 }}>
+                <Box sx={{ width: isMobile ? "90%" : "35%", alignSelf: "flex-start", minHeight: "150px", bgcolor: "background.gray", borderRadius: "10px" }}>
+                    <Typography sx={{ fontSize: "16px", fontWeight: "bold", p: 2 }}>
                         Grammar Check Result
                     </Typography>
                     {!content && (
-                        <Typography sx={{ fontSize: "14px",p:2}}>
+                        <Typography sx={{ fontSize: "14px", p: 2 }}>
                             Paste your CV content and click "Check Grammar" to get started.                            </Typography>
                     )}
                 </Box>
